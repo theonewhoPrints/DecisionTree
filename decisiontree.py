3d189cd3e0c76f6966cfa4d6c8363e77cf9ec03b
ias2858 <131394537+theonewhoPrints@users.noreply.github.com>
2024-11-09 17:44:56 -0500

    changes in dicisiontree

--- decisiontree.py
+++ decisiontree.py
@@ -2,8 +2,12 @@
 This will be the decision tree we are making
 
 """
+import math
 
 import pandas as pd
+from pandas.core.arrays.categorical import contains
+
+
 def read_input():
 
     input_vectors = []
@@ -29,46 +33,152 @@ def read_input():
 
 
 def Decision_tree(dataframe):
-
-
-
-
+    store_remainder = []
     length, width = dataframe.shape
 
-    # for i in range(width - 1):
-    i = 0
-    attribute = str(i)
 
-    true_false_counts = dataframe[attribute].value_counts()
-    true_count = true_false_counts[True]
-    false_count = true_false_counts[False]
+    for i in range(width - 1):
+
+        j = 0
+        i = 0
+        attribute = str(i)
+        true_rows = dataframe[dataframe[attribute] == True].index.tolist()
 
 
-    true_rows = dataframe[dataframe[attribute] == True].index.tolist()
+# A VALUES OF TRUE
+#     true_col_true_A_values = dataframe.loc[
+#     (dataframe.index.isin(true_rows)) & (dataframe['label'] == "A"), 'label'].tolist()
+#     print(len(true_col_true_A_values))
+
+    # Filter to get only rows with True values in the attribute column and label equal to "A"
+    true_rows_with_A = dataframe.loc[(dataframe.index.isin(true_rows)) & (dataframe['label'] == "A")]
+
+    # Create a dictionary with row indices as keys and 'A' as the value
+    true_col_true_A_dict = {index: 'A' for index in true_rows_with_A.index}
+
+    # only by col & row
+
+
+
+
+
+
+
+# B VALUES OF TRUE
+    true_rows_with_B = dataframe.loc[(dataframe.index.isin(true_rows)) & (dataframe['label'] == "B")]
+    true_col_true_B_dict = {index: 'B' for index in true_rows_with_B.index}
+
+
+
+    # should be 88
+##################################################################
+
+# ################## FALSE LISTS ################################################
+
     false_rows = dataframe[dataframe[attribute] == False].index.tolist()
 
+# A VALUES OF FALSE
+#     false_col_true_A_values = dataframe.loc[
+#     (dataframe.index.isin(false_rows)) & (dataframe['label'] == "A"), 'label'].tolist()
 
-    # right now I can use length of list, but later I can use amount only instead of making
-    # 2 new lists. Length(divisor should equal amount of thos trues or those falses.
+    false_rows_with_A = dataframe.loc[(dataframe.index.isin(false_rows)) & (dataframe['label'] == "A")]
+    false_col_true_A_dict = {index: 'A' for index in false_rows_with_A.index}
 
-    last_col_true_values = dataframe.loc[true_rows, 'label'].tolist() #on'es with label = A or B
-    #  THIS ONE FOR NEXT.
+# B VALUES OF FALSE
+#     false_col_true_B_values = dataframe.loc[
+#     (dataframe.index.isin(false_rows)) & (dataframe['label'] == "B"), 'label'].tolist()
 
-
-
-    last_col_false_values = dataframe.loc[false_rows, 'label'].tolist()
-
-
-
-    #Print the lists to verify
-    print("Last column values for rows with True values:", last_col_true_values)
-    print("Last column values for rows with False values:", last_col_false_values)
-    # print(true_rows_A)
+    false_rows_with_B = dataframe.loc[(dataframe.index.isin(false_rows)) & (dataframe['label'] == "B")]
+    false_col_true_B_dict = {index: 'B' for index in false_rows_with_B.index}
 
 
+    ##################################################################
+
+    #lue_rows = dataframe[dataframe['label'] == "A"].index.tolist().  -- the og ones
+    #blue_rows = dataframe[dataframe['label'] == "B"].index.tolist()
+    #print(lue_rows)
+
+
+    P = (len(true_rows)/length) * ( (len(true_col_true_A_dict) * math.log2(1/len(true_col_true_A_dict)))
+    + (len(true_col_true_B_dict) * math.log2(1/len(true_col_true_B_dict)))  )
+
+    H = (len(false_rows) / length) * ((len(false_col_true_A_dict) * math.log2(1 / len(false_col_true_A_dict)))
+                                 + (len(false_col_true_B_dict) * math.log2(1 / len(false_col_true_B_dict))))
+
+
+    r = P + H
+    store_remainder.append(r)
+    ls = store_remainder.index(r)
+
+
+    # store dataframes and cal it the same as the other one,
+    # if not in explored and attributes less than etc
+
+    true_rows_data = dataframe.loc[true_rows]
+    false_rows_data = dataframe.loc[false_rows]
+
+    if  len(store_remainder) >= 2 and store_remainder[ls] <= store_remainder[ls - 1]:
+        #new_data_left = tru
+        depth = attribute
+        store_remainder.clear()
+    #store  dataframes
+    # do left and then right printing afterwards
+    # else:
+    #
+    #     continue
+
+
+    #outside of for range, call print function
+    # recursive call on both left and right side
 
 
 
+    # remainder.clear
+    # with new remainder and it's new true and false rows, rec call on each true and fall
+
+    #too_string()
+
+    #from here we can make a dict, for the too string method(to hashmap)
+
+    # attribute,
+    # if it's in explored
+
+
+    # if not new_data_left:
+
+
+
+    """
+     " the depth is : [
+     
+     
+     values at depth:    
+        A;                    ]
+    
+        B; 
+    
+    """
+
+
+
+
+
+
+
+    # then if lower store those rows in that level(do the math rn)
+
+    # MATH
+    # math.log2()
+
+
+
+
+
+
+
+def too_string():
+
+    pass
 
 
 
